039fe2158a156d17deba0973a1c8f02d271b8121
Benjamin Gretic <bgretic@c3r9p7.42vienna.com>
2025-10-20 17:49:57 +0200

    CPP04 done

--- ex01/Brain.hpp
+++ ex01/Brain.hpp
@@ -15,6 +15,9 @@ class	Brain
 		Brain(const Brain &to_copy);
 		Brain	&operator=(const Brain &to_copy);
 		~Brain();
+
+	void		add_idea(std::string idea);
+	void		print_ideas();
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,52 @@
 
 int	main(void)
 {
-	Animal	*animals[4] = {new Cat(), new Cat(), new Dog(), new Dog()};
+
+///////////////////////////////////////////////////////////////////////////////
+
+	std::cout << "\033[32m" << ":::Standart Test:::" << "\033[0m" << std::endl;
+
+	Animal *animals[4] = {new Dog(), new Dog(), new Cat(), new Cat()};
+
+	std::cout << std::endl;
 
 	for (int i = 0; i < 4; i++)
-		animals[i]->makeSound();
+	{
+		std::cout << "What does the " << animals[i]->getType() << " say?" << std::endl;
+		std::cout << animals[i]->getType() << " goes "; animals[i]->makeSound();
+		
+	}
+
+	std::cout << std::endl;
 
 	for (int i = 0; i < 4; i++)
 		delete animals[i];
 
-	
+	std::cout << std::endl;
+
+///////////////////////////////////////////////////////////////////////////////
+
+	std::cout << "\033[32m" << ":::Deep Copy Test:::" << "\033[0m" << std::endl;
+
+	Cat	catI;
+	Cat	catII;
+
+	catII = catI;
+
+	std::cout << std::endl;
+
+	catI.access_brain()->add_idea("Hello");
+
+	std::cout << "First Cat: " << std::endl; 
+	catI.access_brain()->print_ideas();
+
+	std::cout << std::endl;
+
+	std::cout << "Second Cat: ";
+	catII.access_brain()->print_ideas();
+
+	std::cout << std::endl;
+
+	//Won't work because the class is abstract!
+	// Animal animal;
 }
